513d4507721eae8da8b0c3c8e606913113eedfdd
alejlatorre <alejandro.latorre@pucp.edu.pe>
2022-12-26 10:01:55 +0100

    renamed web_app, updated dependencies, added forms and html templates

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,12 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+app.config['SECRET_KEY'] = '96c2cfcd56e6532d791efe16d6577965d2a39fdc'
+app.config.update(UPLOAD_PATH=os.path.join(dir_path, 'static'))
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,42 @@
-from flask import render_template
+import secrets
+
+from flask import render_template, request
 
 from application import app
+from application.forms import QRCodeData
+from src.engine import QRCodeScanner
 
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template(template_name_or_list='layout.html', title='Home Page')
+
+
+@app.route('/extract_qrcode', methods=['POST', 'GET'])
+def index_page():
+    form = QRCodeData()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            data = form.data.data
+            image_name = f'{secrets.token_hex(10)}.png'
+            qrcode_location = f'{app.config["UPLOAD_PATH"]}/{image_name}'
+
+            try:
+                qrcs = QRCodeData()
+                image = qrcs.read_image(qrcode_location)
+                qr_text = qrcs.extract_info(img=image)
+            except Exception as ex:
+                print(ex)
+            except ValueError as ve:
+                print(ve)
+
+            return render_template(
+                template_name_or_list='extracted_qrcode.html',
+                title='Extracted',
+                image=image_name,
+            )
+    else:
+        return render_template(
+            template_name_or_list='extract_qrcode.html', title='Index Page', form=form
+        )
