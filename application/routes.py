0ae98ffb49adfe39adb9e5a9a7ea27c288fcc28e
alejlatorre <alejandro.latorre@pucp.edu.pe>
2022-12-27 14:06:11 +0100

    added more data

--- application/routes.py
+++ application/routes.py
@@ -10,9 +10,6 @@ from src.engine import QRCodeScanner
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-# %%
-# %%
-
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
